c0472fe76c4afc4a250d986cae942265cd9d8088
BryanElizondo02 <bryanelizondo02@gmail.com>
2021-03-07 18:54:53 -0600

    Login style

--- Web/Controllers/LoginController.cs
+++ Web/Controllers/LoginController.cs
@@ -26,20 +26,20 @@ namespace Web.Controllers
             {
                 /*if (ModelState.IsValid)
                 {*/
-                    oUsuario = _ServiceUsuario.GetUsuario(user.Correo, user.Clave);
+                oUsuario = _ServiceUsuario.GetUsuario(user.Correo, user.Clave);
 
-                    if (oUsuario != null)
-                    {
-                        Session["User"] = oUsuario;
-                        Log.Info($"Accede {oUsuario.Nombre} {oUsuario.Apellido1} con el rol {oUsuario.ROL.Id}-{oUsuario.ROL.Descripcion}");
-                        return RedirectToAction("Index", "Home");
-                    }
-                    else
-                    {
-                        Log.Warn($"{user.Correo} se intentó conectar  y falló");
-                        ViewBag.NotificationMessage = Util.SweetAlertHelper.Mensaje("Login", "Error al autenticarse", SweetAlertMessageType.warning);
+                if (oUsuario != null)
+                {
+                    Session["User"] = oUsuario;
+                    Log.Info($"Accede {oUsuario.Nombre} {oUsuario.Apellido1} con el rol {oUsuario.ROL.Id}-{oUsuario.ROL.Descripcion}");
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    Log.Warn($"{user.Correo} se intentó conectar  y falló");
+                    ViewBag.NotificationMessage = Util.SweetAlertHelper.Mensaje("Login", "Error al autenticarse", SweetAlertMessageType.warning);
 
-                    }
+                }
                 //}
 
                 return View("Index");
@@ -51,7 +51,7 @@ namespace Web.Controllers
                 // Pasar el Error a la página que lo muestra
                 TempData["Message"] = ex.Message;
                 TempData.Keep();
-                return RedirectToAction("Default", "Error");
+                return RedirectToAction("Default", "Login");
             }
         }
 
@@ -77,7 +77,7 @@ namespace Web.Controllers
                 TempData["Message"] = ex.Message;
                 TempData["Redirect"] = "Login";
                 TempData["Redirect-Action"] = "Index";
-                return RedirectToAction("Default", "Error");
+                return RedirectToAction("Default", "Login");
             }
         }
 
@@ -98,10 +98,80 @@ namespace Web.Controllers
                 TempData["Message"] = ex.Message;
                 TempData["Redirect"] = "Login";
                 TempData["Redirect-Action"] = "Index";
-                return RedirectToAction("Default", "Error");
+                return RedirectToAction("Default", "Login");
             }
         }
 
-       
+        public ActionResult Error(Exception ex)
+        {
+            //controller
+            String redirect = "Login";
+            //action controller
+            String redirectAction = "Index";
+
+            HttpException httpException = ex as HttpException;
+            //Obtener el codigo de estado HTTP
+            int error = httpException != null ? httpException.GetHttpCode() : 0;
+
+            switch (error)
+            {
+                case 400:
+                    ViewBag.Title = "Solicitud incorrecta";
+                    ViewBag.Description = "El servidor no pudo interpretar la solicitud dada una sintaxis inválida.";
+                    break;
+
+                case 401:
+                    ViewBag.Title = "No autorizado";
+                    ViewBag.Description = "Es necesario autenticar el usuario para obtener la respuesta solicitada";
+                    break;
+                case 403:
+                    ViewBag.Title = "Acceso restringido";
+                    ViewBag.Description = "El usuario no posee los permisos necesarios para el contenido";
+                    break;
+                default:
+                    ViewBag.Title = error + " Error";
+                    ViewBag.Description = ex.Message;
+                    break;
+            }
+            ViewBag.redirect = redirect;
+            ViewBag.redirectAction = redirectAction;
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+
+        public ActionResult Default()
+        {
+            //Controlador
+            String redirect = "Login";
+            //Acción de controlador
+            String redirectAction = "Index";
+            if (TempData.ContainsKey("Message"))
+            {
+                ViewBag.Title = "Error";
+                ViewBag.Description = TempData["Message"];
+                if (TempData.ContainsKey("Redirect"))
+                {
+                    redirect = (String)TempData["Redirect"];
+                }
+                if (TempData.ContainsKey("Redirect-Action"))
+                {
+                    redirectAction = (String)TempData["Redirect-Action"];
+                }
+                ViewBag.redirect = redirect;
+                ViewBag.redirectAction = redirectAction;
+                return View("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+
     }
 }
